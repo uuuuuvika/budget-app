cf14e65aa6bb9d1a360661470233361968b19e16
uuuuuvika <47716922+uuuuuvika@users.noreply.github.com>
2023-05-01 01:57:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import budget
 from budget import create_spend_chart
 from unittest import main
 
-
 # category 1
 food = budget.Category("Food")
 food.deposit(1000, "initial deposit")
 food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
+food.withdraw(15.89, "restaurant and so on")
 # category 2
 clothing = budget.Category("Clothing")
 food.transfer(50, clothing)
